9870018ec3058429f911ec80bf4355873073f994
Giza-2210 <mahizin255@gmail.com>
2025-11-22 16:10:56 -0600

    Modificación de layaout, css, index

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -8,8 +8,19 @@ namespace Web.Controllers
     {
         public IActionResult Index()
         {
+            // Ejemplos: reemplaza con datos reales desde tu base de datos
+            ViewBag.VentasHoy = 25;
+            ViewBag.MedicamentosProximos = 8;
+            ViewBag.Clientes = 140;
+            ViewBag.Proveedores = 12;
+
+            // Datos para la gráfica
+            ViewBag.LabelsDias = new[] { "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb", "Dom" };
+            ViewBag.VentasPorDia = new[] { 10, 14, 8, 20, 25, 18, 12 };
+
             return View();
         }
+
 
         public IActionResult Privacy()
         {
